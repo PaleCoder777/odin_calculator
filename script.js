a930bd14605f883d6d554959c5feee674438933d
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-05-25 18:16:20 -0400

    Add basic math operator functions

---
+++ script.js
@@ -0,0 +1,18 @@
+// Project: Calculator
+
+// Basic Math Operators Functions
+function mathAdd(num1, num2) {
+    return (num1 + num2);
+}
+
+function mathSubtract(num1, num2) {
+    return (num1 - num2);
+}
+
+function mathMultiply(num1, num2) {
+    return (num1 * num2);
+}
+
+function mathDivide(num1, num2) {
+    return (num1 / num2);
+}
