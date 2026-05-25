64a06568885449b9df38908ca8ffa0c8c48a5fca
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-05-25 18:36:26 -0400

    Add calc calues + operate function

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ function mathMultiply(num1, num2) {
 function mathDivide(num1, num2) {
     return (num1 / num2);
 }
+
+// 3 variables for a calculatior operations
+let calcValue1;
+let calcValue2;
+let calcOperator;
+
+// Calculatore Operate function, calls our math operator functions
+function calcOperate(operator, num1, num2) {
+    if (operator === "+") return mathAdd(num1, num2);
+    if (operator === "-") return mathSubtract(num1, num2);
+    if (operator === "*") return mathMultiply(num1, num2);
+    if (operator === "/") return mathDivide(num1, num2);
+}
